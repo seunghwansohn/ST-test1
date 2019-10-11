7d47d314e1d30c29a5d0f3a6ebfb58dc9cc83f25
Seunghwan <seunghwansohn@gmail.com>
2019-10-11 21:47:32 +0700

    약간 오류

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,70 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+import axios from 'axios';
 
-
-const customers = [
-  {
-    'id': 1,
-    'image': 'https://placeimg.com/64/64/1',
-    'name': '홍길동',
-    'birthday': '961222',
-    'gender': '남자',
-    'job': '대학생'
+const styles = theme => ({
+  root: {
+  width: "100%",
+  marginTop: theme.spacing.unit * 3,
+  overflowX: "auto"
   },
-
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '노시개',
-    'birthday': '961112',
-    'gender': '여자',
-    'job': '무직'
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '노시개',
-    'birthday': '961112',
-    'gender': '여자',
-    'job': '무직'
+  table: {
+  minWidth: 1080
   }
-
-
-
-]
-
-class App extends Component { 
- render() {
-   return (
-     <div>
-       {
-       customers.map(c => {
-         return (
-          <Customer
-          key={c.id}
-          id={c.id}
-          image={c.image}
-          name={c.name}
-          birthday={c.birthday}
-          gender={c.gender}
-          job={c.job}
-          />
-         );
-       })
-       }
-      </div>
-   );
- }
+  });
+  
+class App extends Component {
+  state = {
+    customers: ''
+    }
+    
+    componentDidMount() {
+      this.callApi()
+        .then(res => this.setState({customers: res}))
+        .catch(err => console.log(err));
+    }
+    
+    callApi = async () => {
+    const response = await axios.get('/api/customers');
+    const body = await response.json();
+    return body;
+    }
+    
+    render() {
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+          <TableRow>
+          <TableCell>번호</TableCell>
+          <TableCell>이미지</TableCell>
+          <TableCell>이름</TableCell>
+          <TableCell>생년월일</TableCell>
+          <TableCell>성별</TableCell>
+          <TableCell>직업</TableCell>
+          </TableRow>
+          </TableHead>
+          <TableBody>
+          {this.state.customers ? this.state.customers.map(c => {
+          return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+          }) : ''}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
-
-export default App;
+    
+export default withStyles(styles)(App);
+    
+    
+    
