6cd84586893d8c3942e850a86a33ce49047849f3
Seunghwan <seunghwansohn@gmail.com>
2019-10-12 12:13:51 +0700

    데이터베이스 연동 완료

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
 
-const styles = makeStyles(theme => ({
+const styles = (theme => ({
   root: {
   width: "100%",
   marginTop: theme.spacing(3),
@@ -41,8 +41,9 @@ const styles = makeStyles(theme => ({
   render() {
     const { classes } = this.props;
     return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
+      // <Paper className={classes.root}>
+      //   <Table className={classes.table}>
+      <div><Table>
           <TableHead>
             <TableRow>
               <TableCell align="right">번호</TableCell>
@@ -53,12 +54,14 @@ const styles = makeStyles(theme => ({
               <TableCell align="right">직업</TableCell>
             </TableRow>
           </TableHead>
-          
+          <TableBody>
           {this.state.customers ? this.state.customers.map(c => {
             return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
           }) : ''}
+          </TableBody>
         </Table>
-      </Paper>
+        </div>
+      // </Paper>
 
 
 
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,43 +7,45 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+
+const conf = JSON.parse(data);
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+
+    host: conf.host,
+    
+    user: conf.user,
+    
+    password: conf.password,
+    
+    port: conf.port,
+    
+    database: conf.database
+  
+  });
+  
+  connection.connect();
+  
+  
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '홍길동',
-            'birthday': '961222',
-            'gender': '남자',
-            'job': '대학생'
-          },
-        
-          {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '노시개',
-            'birthday': '961112',
-            'gender': '여자',
-            'job': '무직'
-          },
-          {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '노시개',
-            'birthday': '961112',
-            'gender': '여자',
-            'job': '무직'
-          },
-          {
-            'id': 4,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '노hh시개asdfasdfasdfsadf',
-            'birthday': '981112',
-            'gender': '여자',
-            'job': '무직'
-          }
-    ]);
+  connection.query(
+
+    'SELECT * FROM CUSTOMER',
+    
+    (err, rows, fields) => {
+    
+    res.send(rows);
+    
+    }
+    
+    )
+    
 });
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
